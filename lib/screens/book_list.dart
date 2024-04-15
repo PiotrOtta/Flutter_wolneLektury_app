46b406e1d436b03c3de308ecefccd3fda0cea371
Robert Latoszewski <latoszewskirobert@gmail.com>
2024-04-16 01:45:50 +0200

    Add initial book list element and search bar

--- lib/screens/book_list.dart
+++ lib/screens/book_list.dart
@@ -1,13 +1,24 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:wolne_lektury_client/widgets/book_list_element.dart';
+import 'package:wolne_lektury_client/widgets/search_bar.dart';
 
 class BookListScreen extends StatelessWidget {
   const BookListScreen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Here be dragons within books"),
-    );
+    return Column(children: <Widget>[
+      const SizedBox(height: 20.0),
+      const SizedBox(child: const CustomSearchBar()),
+      Expanded(
+        child: ListView(children: const [
+          BookListElement(bookTitle: "Wiedźmin 1"),
+          BookListElement(bookTitle: "Wiedźmin 2"),
+          BookListElement(bookTitle: "Wiedźmin 3"),
+        ]),
+      )
+    ]);
   }
-
 }

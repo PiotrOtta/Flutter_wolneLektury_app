7e82865d74819754c3c648e8d735a37d59b32f7a
Piotr Otta™ <ottapiotr@gmail.com>
2024-04-20 20:27:52 +0200

    feat: :sparkles: infinite scroll

--- lib/screens/book_list.dart
+++ lib/screens/book_list.dart
@@ -5,6 +5,7 @@ import 'package:wolne_lektury_client/models/book_details_dto.dart';
 import 'package:wolne_lektury_client/services/wolne_lektury_api_connector.dart';
 import 'package:wolne_lektury_client/widgets/book_list_element.dart';
 import 'package:wolne_lektury_client/widgets/search_bar.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class BookListScreen extends StatefulWidget {
   const BookListScreen({super.key});
@@ -14,56 +15,68 @@ class BookListScreen extends StatefulWidget {
 }
 
 class _BookListScreenState extends State<BookListScreen> {
-  List<BookDetailsDto> books = [];
+  static const _pageSize = 10;
+
+  final PagingController<int, BookDetailsDto> _pagingController =
+      PagingController(firstPageKey: 0);
 
   @override
   void initState() {
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
+    });
     super.initState();
-    fetchData();
   }
 
-  Future<void> fetchData() async {
-    try {
-      List<BookDetailsDto> fetchedBooks =
-          await WolneLekturyApiConnector.fetchBooks();
+  int lastValue = 0;
 
-      setState(() {
-        books = fetchedBooks;
-      });
-    } catch (e) {
-      print('Error fetching books: $e');
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      final newItems = (await WolneLekturyApiConnector.fetchBooks(
+              count: pageKey > 0 ? pageKey : 10))
+          .sublist(lastValue);
+      final isLastPage = newItems.length < _pageSize;
+      lastValue = pageKey;
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + newItems.length;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
     }
   }
 
   @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-            child: Stack(
-          children: [
-            ListView.builder(
-              padding: const EdgeInsets.only(top: 60, bottom: 60),
-              itemCount: books.length,
-              itemBuilder: (context, index) {
-                return BookListElement(
-                  author: books[index].author,
-                  bookTitle: books[index].title,
-                  thumbnailUrl: books[index].thumbnailUrl,
-                  fileUrl: books[index].fileUrl,
-                  favorite: BookDetailsDto.bookFavourites.firstWhere(
-                              (element) => element?.title == books[index].title,
-                              orElse: () => null) !=
-                          null
-                      ? true
-                      : false,
-                );
-              },
+    return Stack(
+      children: [
+        PagedListView<int, BookDetailsDto>(
+          padding: const EdgeInsets.only(top: 60, bottom: 60),
+          pagingController: _pagingController,
+          builderDelegate: PagedChildBuilderDelegate<BookDetailsDto>(
+            itemBuilder: (context, item, index) => BookListElement(
+              author: item.author,
+              bookTitle: item.title,
+              thumbnailUrl: item.thumbnailUrl,
+              fileUrl: item.fileUrl,
+              favorite: BookDetailsDto.bookFavourites.firstWhere(
+                          (element) => element?.title == item.title,
+                          orElse: () => null) !=
+                      null
+                  ? true
+                  : false,
             ),
-            const CustomSearchBar(),
-          ],
-        ))
-        // const SizedBox(child: CustomSearchBar()),
+          ),
+        ),
+        const CustomSearchBar(),
       ],
     );
   }

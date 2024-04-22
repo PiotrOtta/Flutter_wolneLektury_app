3dc2be511c1379f4d783e2c72c8eafa6109086ad
Robert Latoszewski <latoszewskirobert@gmail.com>
2024-04-22 21:10:04 +0200

    Adjust searchbar to return hints

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class CustomSearchBar extends StatefulWidget {
   const CustomSearchBar({super.key});
@@ -9,41 +11,78 @@ class CustomSearchBar extends StatefulWidget {
 }
 
 class _CustomSearchBarState extends State<CustomSearchBar> {
+  final TextEditingController _textEditingController = TextEditingController();
+  List<String> _suggestions = [];
 
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _getSuggestions(String query) async {
+    if (query.isNotEmpty) {
+      var uri = 'https://wolnelektury.pl/szukaj/hint/?max=10&term=${Uri.encodeFull(query)}';
+      final response = await http.get(Uri.parse(uri));
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+
+        if (jsonData != null && jsonData is List && jsonData.isNotEmpty) {
+          var bookTitles = jsonData.where((item) => item['type'] == "book").map((item) => item['label']);
+          if (bookTitles.isNotEmpty) {
+            setState(() {
+              _suggestions = List<String>.from(bookTitles);
+            });
+          }
+        } else {
+          setState(() {
+            _suggestions = [];
+          });
+        }
+
+      } else {
+        throw Exception('Failed to load suggestions');
+      }
+    } else {
+      setState(() {
+        _suggestions = [];
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: SearchAnchor(
-          builder: (BuildContext context, SearchController controller) {
-        return SearchBar(
-          controller: controller,
-          padding: const MaterialStatePropertyAll<EdgeInsets>(
-              EdgeInsets.symmetric(horizontal: 16.0)),
-          onTap: () {
-            controller.openView();
-          },
-          onChanged: (_) {
-            controller.openView();
-          },
-          leading: const Icon(Icons.search),
-        );
-      }, suggestionsBuilder:
-              (BuildContext context, SearchController controller) {
-        return List<ListTile>.generate(5, (int index) {
-          final String item = 'item $index';
-          return ListTile(
-            title: Text(item),
+        viewOnChanged: (value) {
+          _getSuggestions(value);
+        },
+        builder: (BuildContext context, SearchController controller) {
+          return SearchBar(
+            // controller: _textEditingController,
+            padding: const MaterialStatePropertyAll<EdgeInsets>(
+              EdgeInsets.symmetric(horizontal: 16.0),
+            ),
             onTap: () {
-              setState(() {
-                controller.closeView(item);
-              });
+              controller.openView();
             },
+            leading: const Icon(Icons.search),
           );
-        });
-      }),
+        },
+        suggestionsBuilder: (BuildContext context, SearchController controller) {
+          return _suggestions.map((label) {
+            return ListTile(
+              title: Text(label),
+              onTap: () {
+                setState(() {
+                  controller.closeView(label);
+                });
+              },
+            );
+          }).toList();
+        },
+      ),
     );
   }
-
 }
